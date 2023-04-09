133e44ccd45f9fce23d39753aac7ce143bd29892
elbabili <elbabilimohamed@gmail.com>
2023-04-09 13:34:34 +0200

    extract JQuery

--- src/model.js
+++ src/model.js
@@ -1,15 +1,15 @@
 
-//Represente un produit minifié servant à une commande, utile côté back pour associer une commande avec les produits concernés
-class ProductItem {
+//Represente un article minifié servant à une commande, utile côté back pour associer une commande avec les articles concernés
+class ArticleItem {
     constructor(id,name,quantity){
         this.id = id;   this.name = name; this.quantity = quantity;
     }
 }
 
-//Represente un produit
-class Product {
-    constructor(id, name, brand, price, quantity) {
-        this.id = id;   this.name = name;   this.brand = brand;   this.price = price;   this.quantity = quantity;
+//Represente un article
+class Article {
+    constructor(id, name, brand, price, quantity, categorie) {
+        this.id = id;   this.name = name;   this.brand = brand;   this.price = price;   this.quantity = quantity ; this.categorie = categorie;
     }
 
     getId() { return this.id; }
@@ -18,6 +18,7 @@ class Product {
     getPrice() { return this.price; }
     getQuantity() { return this.quantity; }
     setQuantity(quantity) { this.quantity = quantity; }
+    getCategorie() { return this.categorie; }
 
     toString() {
         return " id:" + this.id + " " + this.name + " " + this.brand + " " + this.price + "€";
@@ -36,4 +37,27 @@ class Categorie {
     toString() {
         return " id:" + this.id + " " + this.name + " " + this.description;
     }
+}
+
+class Order {
+    constructor(id, date, amount, customer, articleItems) {
+        this.id = id; this.date = date; this.amount = amount; this.customer=customer, this.articleItems = articleItems;
+    }
+
+    getId() { return this.id; }
+    getDate() { return this.date; }
+    getAmount() { return this.amount; }
+    getCustomer() { return this.customer; }
+    getArticleItems() { return this.articleItems; }
+}
+
+class Customer {
+    constructor(name, address, phone, email) {
+        this.name = name; this.address = address; this.phone = phone; this.email=email;
+    }
+
+    getName() { return this.name; }
+    getAddress() { return this.address; }
+    getPhone() { return this.phone; }
+    getEmail() { return this.email; }
 }
